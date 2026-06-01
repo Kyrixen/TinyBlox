9ee50a94f1d4ec019a7f14e431d4531acd95dc4c
Kyrixen <kyrixen@atomicmail.io>
2026-06-01 08:28:21 +0000

    Added SoundID

---
+++ core/src/main/java/io/kyrixen/tinyblox/sound/SoundID.java
@@ -0,0 +1,58 @@
+package io.kyrixen.tinyblox.sound;
+
+import java.util.Objects;
+
+public class SoundID {
+    
+    // Sound type enum helper
+    public enum SoundType {
+        UI,
+        GAME,
+        HUD,
+        MISC
+    }
+
+    // ID vars //
+
+    private final String namespace;
+    private final SoundType type;
+    private final String id;
+
+    // SoundID constructor
+    public SoundID(String namespace, SoundType type, String id) {
+        this.namespace = namespace;
+        this.type = type;
+        this.id = id;
+    }
+
+    // Getters //
+
+    public String getNamespace() { return this.namespace; }
+    public String getID() { return this.id; }
+    public SoundType getType() { return this.type; }
+
+    // Overrides //
+
+    @Override
+    public boolean equals(Object o) {
+        
+        if(o == null) return false;
+        if(!(o instanceof SoundID)) return false;
+
+        SoundID other = (SoundID) o;
+
+        return this.namespace.equals(other.getNamespace()) && this.type.equals(other.getType()) && this.id.equals(other.getID());
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(namespace, type, id);
+    }
+
+    @Override
+    public String toString() {
+        return this.namespace + ":" + this.type.name().toLowerCase() + ":" + this.id;
+    }
+
+}
